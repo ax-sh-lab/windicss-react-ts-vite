5d45170e2a3f87dc5104c759d78251fd9d3b5345
Axm <56112475+ax-sh@users.noreply.github.com>
2021-12-27 13:55:27 +0545

    feat: add product-page

--- src/components/pages/ProductPage/ProductPage.tsx
+++ src/components/pages/ProductPage/ProductPage.tsx
@@ -1,7 +1,23 @@
+import React from "react";
+
 const ProductPage = () => {
+  const [show, hide] = React.useState(true);
   return (
     <div className="ProductPage">
-      <section className="bg-green-400 h-screen"></section>
+      <section className="h-screen flex">
+        <div className="flex h-full w-2/3">
+          <div className="flex-1"></div>
+          <div className="bg-gray-300 w-10 grid place-items-center">
+            <span
+              className="cursor-pointer transform -rotate-90"
+              onClick={() => hide(!show)}
+            >
+              {show ? "HIDE" : "SHOW"}
+            </span>
+          </div>
+        </div>
+        <div className="bg-green-400 h-full w-1/3"></div>
+      </section>
     </div>
   );
 };
