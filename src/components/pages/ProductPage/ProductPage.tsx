0a58c16b63d8a572965e66a6c8c074814a12635c
Axm <56112475+ax-sh@users.noreply.github.com>
2021-12-27 16:08:22 +0545

    add logic for hidding ui

--- src/components/pages/ProductPage/ProductPage.tsx
+++ src/components/pages/ProductPage/ProductPage.tsx
@@ -1,31 +1,53 @@
 import classNames from "classnames";
 import React from "react";
 
+const ToggleButton = ({
+  setHide,
+}: {
+  setHide: React.SetStateAction<boolean>;
+}) => {
+  const [show, hide] = React.useState(false);
+  React.useEffect(() => {
+    setHide(show);
+  }, [setHide, show]);
+  return (
+    <div className="bg-white shadow grid place-content-center">
+      <span
+        className="cursor-pointer transform -rotate-90"
+        onClick={() => hide(!show)}
+      >
+        {show ? "SHOW" : "HIDE"}
+      </span>
+    </div>
+  );
+};
+
 const ProductPage = () => {
   const [show, hide] = React.useState(true);
   const uiClasses = classNames("bg-green-400 h-full w-1/3 transition-all", {
-    "w-0": !show,
+    "transform translate-x-full": !show,
   });
   return (
     <div className="ProductPage">
-      <section className="h-screen flex">
-        <div
-          className={classNames("flex h-full  transition-all", {
-            "w-2/3": show,
-            "w-full": !show,
-          })}
+      <section className={"flex min-h-screen overflow-hidden"}>
+        <main className="flex flex-grow bg-green-300">
+          <div className="flex-grow bg-red-400">main</div>
+          <ToggleButton setHide={hide} />
+        </main>
+        {/*  translate-x-full md:translate-x-0  */}
+        <aside
+          className={classNames(
+            "sidebar w-100 transition-all transition-transform duration-300 ease-in text-white bg-gray-900",
+            show && "-mr-100"
+          )}
         >
-          <div className="flex-1"></div>
-          <div className="bg-gray-300 w-10 grid place-items-center">
-            <span
-              className="cursor-pointer transform -rotate-90"
-              onClick={() => hide(!show)}
-            >
-              {show ? "HIDE" : "SHOW"}
-            </span>
-          </div>
-        </div>
-        <div className={uiClasses}></div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, ducimus
+            nemo saepe exercitationem dignissimos nulla sequi. Exercitationem,
+            alias odio! Expedita sequi tempora excepturi hic. Labore recusandae
+            accusamus aperiam dignissimos ratione!
+          </p>
+        </aside>
       </section>
     </div>
   );
