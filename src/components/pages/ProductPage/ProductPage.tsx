59634fc40edae56f33ead4defe59881de59bb2b3
Axm <56112475+ax-sh@users.noreply.github.com>
2021-12-27 14:35:55 +0545

    add toggle logic

--- src/components/pages/ProductPage/ProductPage.tsx
+++ src/components/pages/ProductPage/ProductPage.tsx
@@ -1,11 +1,20 @@
+import classNames from "classnames";
 import React from "react";
 
 const ProductPage = () => {
   const [show, hide] = React.useState(true);
+  const uiClasses = classNames("bg-green-400 h-full w-1/3 transition-all", {
+    "w-0": !show,
+  });
   return (
     <div className="ProductPage">
       <section className="h-screen flex">
-        <div className="flex h-full w-2/3">
+        <div
+          className={classNames("flex h-full  transition-all", {
+            "w-2/3": show,
+            "w-full": !show,
+          })}
+        >
           <div className="flex-1"></div>
           <div className="bg-gray-300 w-10 grid place-items-center">
             <span
@@ -16,7 +25,7 @@ const ProductPage = () => {
             </span>
           </div>
         </div>
-        <div className="bg-green-400 h-full w-1/3"></div>
+        <div className={uiClasses}></div>
       </section>
     </div>
   );
