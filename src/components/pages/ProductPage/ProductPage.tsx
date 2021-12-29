e19209e851d62de1c6b86b5d0501f6a7fbc8660d
Axm <56112475+ax-sh@users.noreply.github.com>
2021-12-29 14:28:40 +0545

    feat: add border toggle on select

--- src/components/pages/ProductPage/ProductPage.tsx
+++ src/components/pages/ProductPage/ProductPage.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
-import React from "react";
+import React, { Fragment } from "react";
+import faker from "faker";
 
 const ToggleButton = ({ setHide }: { setHide: (state: boolean) => void }) => {
   const [show, hide] = React.useState(false);
@@ -18,16 +19,93 @@ const ToggleButton = ({ setHide }: { setHide: (state: boolean) => void }) => {
   );
 };
 
+function ProfileCard({ id, image_url, label }: any) {
+  return (
+    <label className={"profile inline-block h-30 w-50 m-2"}>
+      <input className={"hidden "} type={"radio"} name={"profile"} value={id} />
+      <div
+        className={
+          "card cursor-pointer relative h-full w-full rounded-md bg-black rounded-md border-transparent overflow-hidden"
+        }
+      >
+        <img
+          alt={"card"}
+          className={"absolute h-full w-full object-fill rounded-md"}
+          src={image_url}
+        />
+        <h6>{label}</h6>
+      </div>
+    </label>
+  );
+}
+
+function getProduct() {
+  const { productName, product, productAdjective, productDescription } =
+    faker.commerce;
+
+  console.log(product(), productAdjective(), productDescription());
+  // , product
+  //     // color: ƒ ()
+  //     // department: ƒ ()
+  //     // price: ƒ (min, max, dec, symbol)
+  //     // product: ƒ ()
+  //     // productAdjective: ƒ ()
+  //     // productDescription: ƒ ()
+  //     // productMaterial: ƒ ()
+  //     // productName: ƒ ()
+  //     //     [[Prototype]]: Object
+  return {
+    label: productName(),
+    // image_url: faker.image.fashion(),
+    image_url: "https://source.unsplash.com/xJkTCbtuqAY",
+  };
+}
+
+function Profiles({ children }) {
+  function handleChange({ target }: any) {
+    const profileID = target.value;
+    const product = getProduct();
+
+    console.log("change", profileID, product);
+  }
+
+  return (
+    <form
+      className={"profiles relative whitespace-nowrap overflow-auto p-4"}
+      onChange={handleChange}
+    >
+      {children}
+    </form>
+  );
+}
+
+function Description() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, ducimus nemo
+      saepe exercitationem dignissimos nulla sequi. Exercitationem, alias odio!
+      Expedita sequi tempora excepturi hic. Labore recusandae accusamus aperiam
+      dignissimos ratione!
+    </p>
+  );
+}
+const getProfileItems = () => {
+  return [1, 2, 3, 4, 5].map((i) => {
+    return { id: i, ...getProduct() };
+  });
+};
 const ProductPage = () => {
   const [show, hide] = React.useState(true);
+  const data = JSON.stringify({ ds: "ko" });
+  const [profileItems] = React.useState(getProfileItems());
   // const uiClasses = classNames("bg-green-400 h-full w-1/3 transition-all", {
   //   "transform translate-x-full": !show,
   // });
   return (
     <div className="ProductPage">
-      <section className={"flex min-h-screen overflow-hidden"}>
-        <main className="flex flex-grow bg-green-300">
-          <div className="flex-grow bg-red-400">main</div>
+      <section className={"flex min-h-screen overflow-hidden  transition-all "}>
+        <main className="flex flex-grow bg-green-300 transition-all ">
+          <div className="flex-grow bg-red-400">{data}</div>
           <ToggleButton setHide={hide} />
         </main>
         {/*  translate-x-full md:translate-x-0  */}
@@ -37,12 +115,18 @@ const ProductPage = () => {
             show && "-mr-100"
           )}
         >
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, ducimus
-            nemo saepe exercitationem dignissimos nulla sequi. Exercitationem,
-            alias odio! Expedita sequi tempora excepturi hic. Labore recusandae
-            accusamus aperiam dignissimos ratione!
-          </p>
+          <div className={"p-8"}>
+            <Description />
+            <Profiles>
+              {profileItems.map((i) => {
+                return (
+                  <Fragment key={i.id}>
+                    <ProfileCard {...i} />
+                  </Fragment>
+                );
+              })}
+            </Profiles>
+          </div>
         </aside>
       </section>
     </div>
